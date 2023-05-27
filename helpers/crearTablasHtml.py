f977223dd4a494fcde2d87696c5f1d3a5fdfb1aa
susana <sagiraldo1992@gmail.com>
2023-05-27 13:00:54 -0500

    27-05-2023

---
+++ helpers/crearTablasHtml.py
@@ -0,0 +1,24 @@
+def crearTabla(dataFrame,nombreTabla):
+              archivoHTML = dataFrame.to_html()#Qui tengo el data frame vHTML
+              archivo = open(f"./table/{nombreTabla}.html","w") #tengo un archivo html vacio
+              archivo.write(''' 
+                            
+                         <!DOCTYPE html>
+                         <html>
+                         <head>
+                          <meta charset="UTF-8">
+                         <title>tablas</title>
+                         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous"> 
+                         </head>
+                         <body> 
+                            
+                            ''')
+              archivo.write(archivoHTML)
+              archivo.write(''' 
+                            
+                            </body>
+                            </html>
+
+                            ''')
+              archivo.close()
+              
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from data.apartamentos import apartamento1,apartamento2
+from helpers.crearTablasHtml import crearTabla
 import pandas as pd
 
 #Crear Dataframe
@@ -6,12 +7,16 @@ tabla1 = pd.DataFrame(apartamento1,columns=['edades'])
 tabla2 = pd.DataFrame(apartamento2,columns=['edades'])
 tabla3 = pd.read_csv("./data/empleados.csv")
 
-estadisticasAPTO1 = tabla1.describe()
-estadisticasAPTO2 = tabla2.describe()
-estadisticasEmpleados = tabla3.describe()
+#Efectuando filtros con python
 
-print(estadisticasAPTO1)
-print("\n")
-print(estadisticasAPTO2)
-print("\n")
-print(estadisticasEmpleados)
+#1.Condicion logica
+empladosJovenesAnalista1 = tabla3.query('edad < 28 and cargo == "analista1"')
+empleadosSalarioBajo = tabla3.query('salario < 5000000 and cargo == "analista2"')
+empleadosADespedir = tabla3.query('edad <= 50')
+
+#2. Creamos la tabla HTML con el html del frito
+crearTabla(empladosJovenesAnalista1,"tablaJovenes")
+crearTabla(empleadosSalarioBajo,"tablabajossalarios")
+crearTabla(empleadosADespedir,"tablaoprtunidaddemejora")
+
+
